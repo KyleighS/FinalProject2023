a2a2ccff4ce615ec3ab1adfec0332bf5b821db9d
KyleighS <111814352+KyleighS@users.noreply.github.com>
2023-04-16 17:21:22 -0400

    AI tracking and started shooting

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public float damage = 2f;
+    public float range = 10f;
+    public Camera fpsCam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+        }
+
+    }
+}
